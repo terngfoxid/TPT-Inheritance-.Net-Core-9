3afeafba94a7530cf11e964abd9badd811e88202
terngfoxid <terngfoxid@live.jp>
2025-08-18 15:07:04 +0700

    [fix]git ignore

---
+++ Inheritance_Test/Models/ComponentMetadata.cs
@@ -0,0 +1,35 @@
+﻿using Inheritance_Test.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inheritance_Test.Models
+{
+    public partial class Component
+    {
+        protected Component() {
+            this.Type = this.GetType().FullName;
+        }
+
+        private CustomContext db = new CustomContext();
+        public Component create()
+        {
+            db.Add(this);
+            db.SaveChanges();
+            SetIdValue();
+            return this;
+        }
+
+        public Component update()
+        {
+            db.Update(this);
+            db.SaveChanges();
+            SetIdValue();
+            return this;
+        }
+
+        public virtual void SetIdValue()
+        {
+            Component component = (Component)this;
+            this.Id = component.Id;
+        }
+    }
+}
